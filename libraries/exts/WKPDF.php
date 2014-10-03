57417974d3350df4e2772800e01f556d552cbfaa
root <root@Crei-App1.cs3ae5cloud.internal>
2014-10-04 00:03:25 +0900

    modify PDF library

--- libraries/exts/WKPDF.php
+++ libraries/exts/WKPDF.php
@@ -1,4 +1,4 @@
-<?php
+<?hh
         // Automated configuration. Modify these if they fail. (they shouldn't ;) )
         //$GLOBALS['WKPDF_BASE_PATH']=str_replace(str_replace('\\','/',getcwd().'/'),'',dirname(str_replace('\\','/',__FILE__))).'/';
         $GLOBALS['WKPDF_BASE_PATH']="/var/www/crei/var/pdf";
@@ -36,75 +36,84 @@
                 /**
                  * Private use variables.
                  */
-                private $html='';
-                private $cmd='';
-                private $tmp='';
-                private $pdf='';
-                private $status='';
-                private $orient='Portrait';
-                private $size='A4';
-                private $toc=false;
-                private $copies=1;
-                private $grayscale=false;
-                private $title='';
-                private static $cpu='';
+                private string $html='';
+                private string $cmd='';
+                private string $tmp='';
+                private string $pdf='';
+                private string $status='';
+                private string $orient='Portrait';
+                private string $size='A4';
+                private bool $toc=false;
+                private int $copies=1;
+                private bool $grayscale=false;
+                private string $title='';
+                private string $cpu='';
                 /**
                  * Advanced execution routine.
                  * @param string $cmd The command to execute.
                  * @param string $input Any input not in arguments.
                  * @return array An array of execution data; stdout, stderr and return "error" code.
                  */
-                private static function _pipeExec($cmd,$input=''){
-                        $proc=proc_open($cmd,array(0=>array('pipe','r'),1=>array('pipe','w'),2=>array('pipe','w')),$pipes);
-                        fwrite($pipes[0],$input);
-                        fclose($pipes[0]);
-                        $stdout=stream_get_contents($pipes[1]);
-                        fclose($pipes[1]);
-                        $stderr=stream_get_contents($pipes[2]);
-                        fclose($pipes[2]);
-                        $rtn=proc_close($proc);
-                        return array(
-                                        'stdout'=>$stdout,
-                                        'stderr'=>$stderr,
-                                        'return'=>$rtn
-                                );
+                private function _pipeExec(string $cmd, ?string $input='') {
+                    $pipes = array();
+                    $proc = proc_open($cmd." &", array(0=>array('pipe','r'),1=>array('pipe','w'),2=>array('pipe','w')), $pipes);
+                    fwrite($pipes[0], $input);
+                    fclose($pipes[0]);
+                    $stdout=stream_get_contents($pipes[1]);
+                    fclose($pipes[1]);
+                    $stderr=stream_get_contents($pipes[2]);
+                    fclose($pipes[2]);
+
+                    $proc_status = proc_get_status($proc);
+                    exec('kill -9 '.$proc_status['pid']);
+                    $rtn=proc_close($proc);
+
+                    return array(
+                        'stdout'=>$stdout,
+                        'stderr'=>$stderr,
+                        'return'=>$rtn
+                    );
                 }
                 /**
                  * Function that attempts to return the kind of CPU.
                  * @return string CPU kind ('amd64' or 'i386').
                  */
-                private static function _getCPU(){
+                 /*
+                private function _getCPU(){
                         if(self::$cpu==''){
-                                if(`grep -i amd /proc/cpuinfo`!='')                     self::$cpu='amd64';
+                                if(`grep -i amd /proc/cpuinfo`!=''){
+                                     self::$cpu='amd64';
+                                }
                                 elseif(`grep -i intel /proc/cpuinfo`!='')       self::$cpu='i386';
                                 else throw new Exception('WKPDF couldn\'t determine CPU ("'.`grep -i vendor_id /proc/cpuinfo`.'").');
                         }
                         return self::$cpu;
                 }
+                */
                 /**
                  * Force the client to download PDF file when finish() is called.
                  */
-                public static $PDF_DOWNLOAD='D';
+                public $PDF_DOWNLOAD='D';
                 /**
                  * Returns the PDF file as a string when finish() is called.
                  */
-                public static $PDF_ASSTRING='S';
+                public $PDF_ASSTRING='S';
                 /**
                  * When possible, force the client to embed PDF file when finish() is called.
                  */
-                public static $PDF_EMBEDDED='I';
+                public $PDF_EMBEDDED='I';
                 /**
                  * PDF file is saved into the server space when finish() is called. The path is returned.
                  */
-                public static $PDF_SAVEFILE='F';
+                public $PDF_SAVEFILE='F';
                 /**
                  * PDF generated as landscape (vertical).
                  */
-                public static $PDF_PORTRAIT='Portrait';
+                public $PDF_PORTRAIT='Portrait';
                 /**
                  * PDF generated as landscape (horizontal).
                  */
-                public static $PDF_LANDSCAPE='Landscape';
+                public $PDF_LANDSCAPE='Landscape';
                 /**
                  * Constructor: initialize command line and reserve temporary file.
                  */
@@ -123,23 +132,23 @@
                  * By default orientation is portrait.
                  * @param string $mode Use constants from this class.
                  */
-                public function set_orientation($mode){
-                        $this->orient=$mode;
+                public function set_orientation(string $mode) : void {
+                        $this->orient = $mode;
                 }
                 /**
                  * Set page/paper size.
                  * By default page size is A4.
                  * @param string $size Formal paper size (eg; A4, letter...)
                  */
-                public function set_page_size($size){
-                        $this->size=$size;
+                public function set_page_size(string $size) : void {
+                        $this->size = $size;
                 }
                 /**
                  * Whether to automatically generate a TOC (table of contents) or not.
                  * By default TOC is disabled.
                  * @param boolean $enabled True use TOC, false disable TOC.
                  */
-                public function set_toc($enabled){
+                public function set_toc(bool $enabled) : void {
                         $this->toc=$enabled;
                 }
                 /**
@@ -147,15 +156,16 @@
                  * By default it is one.
                  * @param integer $count Number of page copies.
                  */
-                public function set_copies($count){
+                public function set_copies(int $count) : void {
                         $this->copies=$count;
                 }
+
                 /**
                  * Whether to print in grayscale or not.
                  * By default it is OFF.
                  * @param boolean True to print in grayscale, false in full color.
                  */
-                public function set_grayscale($mode){
+                public function set_grayscale(bool $mode) : void {
                         $this->grayscale=$mode;
                 }
                 /**
@@ -163,57 +173,62 @@
                  * By default it is empty.
                  * @param string Title text.
                  */
-                public function set_title($text){
+                public function set_title(string $text) : void {
                         $this->title=$text;
                 }
                 /**
                  * Set html content.
                  * @param string $html New html content. It *replaces* any previous content.
                  */
-                public function set_html($html){
+                public function set_html(string $html) : void {
                         $this->html=$html;
-                        file_put_contents($this->tmp,$html);
+                        file_put_contents($this->tmp, $html);
                 }
                 /**
                  * Returns WKPDF print status.
                  * @return string WPDF print status.
                  */
-                public function get_status(){
+                public function get_status() : string {
                         return $this->status;
                 }
                 /**
                  * Attempts to return the library's full help.
                  * @return string WKHTMLTOPDF HTML help.
                  */
-                public function get_help(){
+                 /*
+                public function get_help() : string {
                         $tmp=self::_pipeExec('"'.$this->cmd.'" --extended-help');
                         return $tmp['stdout'];
                 }
+                */
                 /**
                  * Convert HTML to PDF.
+                                o' --lowquality'  
                  */
-                public function render(){
-                        //$web=$GLOBALS['WKPDF_BASE_SITE'].$GLOBALS['WKPDF_BASE_PATH'].'tmp/'.basename($this->tmp);
-                        $web=$GLOBALS['WKPDF_BASE_SITE'].'/'.basename($this->tmp);
-                        //$web="/var/www/crei/var/pdf/".basename($this->tmp);
-                        $this->pdf=self::_pipeExec(
-                                '/usr/bin/xvfb-run -a -s "-screen 0, 1024x680x24" '.$this->cmd.' -q'
-                               // .(($this->copies>1)?' --copies '.$this->copies:'')                              // number of copies
-                                .' --zoom 2.5'                                                                // zoom
-                                .' --margin-bottom 19.8'  
-                                .' --orientation '.$this->orient                                                                // orientation
-                                .' --page-size '.$this->size                                                                    // page size
-                               // .($this->toc?' --toc':'')                                                                               // table of contents
-                                .($this->grayscale?' --grayscale':'')                                                   // grayscale
-                                .(($this->title!='')?' --title "'.$this->title.'"':'')                  // title
-                                .' "'.$web.'" -'                                                                                                // URL and optional to write to STDOUT
-                        );
-                        if(strpos(strtolower($this->pdf['stderr']),'error')!==false)throw new Exception('WKPDF system error: <pre>'.$this->pdf['stderr'].'</pre>');
-                        if($this->pdf['stdout']=='')throw new Exception('WKPDF didn\'t return any data. <pre>'.$this->pdf['stderr'].'</pre>');
-                        if(((int)$this->pdf['return'])>1)throw new Exception('WKPDF shell error, return code '.(int)$this->pdf['return'].'.');
-                        $this->status=$this->pdf['stderr'];
-                        $this->pdf=$this->pdf['stdout'];
-                        unlink($this->tmp);
+                public function render($web) : void {
+                    //$web=$GLOBALS['WKPDF_BASE_SITE'].$GLOBALS['WKPDF_BASE_PATH'].'tmp/'.basename($this->tmp);
+                    //$web=$GLOBALS['WKPDF_BASE_SITE'].'/'.basename($this->tmp);
+                    //$web="/var/www/crei/var/pdf/".basename($this->tmp);
+                    $pdfData = $this->_pipeExec(
+                            '/usr/bin/xvfb-run -a -s "-screen 0, 1024x680x24" '.$this->cmd.' -q'
+                           // .(($this->copies>1)?' --copies '.$this->copies:'')                    // number of copies
+                            .' --zoom 2.5'                                                          // zoom
+                            .' --lowquality' 
+                            .' --margin-bottom 19.8'  
+                            .' --orientation '.$this->orient                                        // orientation
+                            .' --page-size '.$this->size                                            // page size
+                           // .($this->toc?' --toc':'')                                             // table of contents
+                            .($this->grayscale?' --grayscale':'')                                   // grayscale
+                            .(($this->title!='')?' --title "'.$this->title.'"':'')                  // title
+                            .' "'.$web.'" -'                                                        // URL and optional to write to STDOUT
+                    );
+
+                    if(strpos(strtolower($pdfData['stderr']),'error')!==false)throw new Exception('WKPDF system error: <pre>'.$pdfData['stderr'].'</pre>');
+                    if($pdfData['stdout']=='')throw new Exception('WKPDF didn\'t return any data. <pre>'.$pdfData['stderr'].'</pre>');
+                    if(((int)$pdfData['return'])>1)throw new Exception('WKPDF shell error, return code '.(int)$pdfData['return'].'.');
+                    $this->status=$pdfData['stderr'];
+                    $this->pdf=$pdfData['stdout'];
+                    unlink($this->tmp);
                 }
                 /**
                  * Return PDF with various options.
@@ -221,9 +236,9 @@
                  * @param string $file The PDF's filename (the usage depends on $mode.
                  * @return string|boolean Depending on $mode, this may be success (boolean) or PDF (string).
                  */
-                public function output($mode, $file=null){
+                public function output(string $mode, ?string $file=null){
                         switch($mode){
-                                case self::$PDF_DOWNLOAD:
+                                case $this->PDF_DOWNLOAD:
                                         if(!headers_sent()){
                                                 header('Content-Description: File Transfer');
                                                 header('Cache-Control: public, must-revalidate, max-age=0'); // HTTP/1.1
@@ -244,10 +259,10 @@
                                                 throw new Exception('WKPDF download headers were already sent.');
                                         }
                                         break;
-                                case self::$PDF_ASSTRING:
+                                case $this->PDF_ASSTRING:
                                         return $this->pdf;
                                         break;
-                                case self::$PDF_EMBEDDED:
+                                case $this->PDF_EMBEDDED:
                                         if(!headers_sent()){
                                                 $this->pdf = str_replace('#00', '', $this->pdf);
                                                 header('Content-Type: application/pdf');
@@ -262,7 +277,7 @@
                                                 throw new Exception('WKPDF embed headers were already sent.');
                                         }
                                         break;
-                                case self::$PDF_SAVEFILE:
+                                case $this->PDF_SAVEFILE:
                                         return file_put_contents($file,$this->pdf);
                                         break;
                                 default:
@@ -271,5 +286,3 @@
                         return false;
                 }
         }
-?>
-
